1c9c9e82b258fbd1ba0a1f5d96ce16138a9eaf92
laugh0608 <baihongjie0626@163.com>
2020-07-30 17:08:57 +0800

    svg不生效，删除

--- js/index.js
+++
@@ -1,3 +0,0 @@
-$(".card-info-social-icons").append(
-    '<a class="social-icon" href="https://space.bilibili.com/146759222" target="_blank"><svg class="icon" aria-hidden="true"><use xlink:href="#icon-bilibili"></use></svg></a>'
-);
